52e0b77e1125f0dd741f8cd48a5f6542b7a0ea19
abk237 <abk@gmail.com>
2023-01-19 11:23:07 +0100

    first commit

---
+++ src/main/java/com/abk/ebanking_api/EbankingApiApplication.java
@@ -0,0 +1,14 @@
+package com.abk.ebanking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbankingApiApplication.class, args);
+		System.out.println("hello wprld");
+	}
+
+}
---
+++ src/test/java/com/abk/ebanking_api/EbankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abk.ebanking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
