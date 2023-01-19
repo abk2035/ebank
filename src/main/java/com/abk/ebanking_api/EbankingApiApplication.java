634c0fb9a1800ca3a1eae917c72208385a121ee7
abk237 <abk@gmail.com>
2023-01-19 11:35:23 +0100

    first commit

--- src/main/java/com/abk/ebanking_api/EbankingApiApplication.java
+++ src/main/java/com/abk/ebanking_api/EbankingApiApplication.java
@@ -8,7 +8,7 @@ public class EbankingApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EbankingApiApplication.class, args);
-		System.out.println("hello wprld");
+		System.out.println("hello wppush rld");
 	}
 
 }
