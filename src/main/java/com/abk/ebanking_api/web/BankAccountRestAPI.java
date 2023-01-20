232910242347a4ea8ccae06b53a946088dd1fbe3
abk237 <abk@gmail.com>
2023-01-20 15:45:58 +0100

    add BankAccountRestAPI start

---
+++ src/main/java/com/abk/ebanking_api/web/BankAccountRestAPI.java
@@ -0,0 +1,11 @@
+package com.abk.ebanking_api.web;
+
+import com.abk.ebanking_api.services.BankAccountService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BankAccountRestAPI {
+    private BankAccountService bankAccountService;
+
+
+}
